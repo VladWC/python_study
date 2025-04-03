3bdcafd94a91f1b1834b3803f11037bbb3910cd6
VladWC <vlad.filin.92@inbox.ru>
2025-04-03 09:17:26 +0300

    start

---
+++ para8.py
@@ -0,0 +1,12 @@
+import random
+
+stroka_sum = []
+
+columm_sum = []
+
+user_answer = int(input("1 - вывести сумму строки. \n 2 - вывести сумму столбца."))
+
+if user_answer == 1:
+    print(stroka_sum)
+elif user_answer == 2:
+    print(columm_sum)
